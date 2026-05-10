00ec06b34502599dc28f2a1d2eab736b64a41514
grae ceney <gceney7@gmail.com>
2026-05-10 18:15:31 +0100

    implemented the memory related stuff into the bootloader of the kernel

--- kernel.c
+++ kernel.c
@@ -1,10 +1,13 @@
 #include "drivers/terminal.h"
 #include "gdt.h"
 #include "idt.h"
+#include "mem/kmalloc.h"
+#include "mem/pmm.h"
 #include "timer.h"
 #include <stdint.h>
 
 void boot(void);
+extern uint8_t __kernel_end;
 
 void kmain(void) {
   boot();
@@ -16,11 +19,26 @@ void kmain(void) {
 }
 
 void boot(void) {
+  const uint32_t total_memory_bytes = PMM_MAX_MEMORY_BYTES;
+  const uint32_t heap_size_bytes = 1024u * 1024u;
+
   terminal_initialize();
   terminal_write("Atomix OS Starting...\n");
 
   init_gdt();
   init_idt();
+
+  pmm_init(total_memory_bytes, (uint32_t)(uintptr_t)&__kernel_end);
+  uint32_t heap_start = pmm_alloc_frame();
+  if (heap_start != 0) {
+    uint32_t pages = heap_size_bytes / PMM_FRAME_SIZE;
+    if ((heap_size_bytes % PMM_FRAME_SIZE) != 0) {
+      pages++;
+    }
+    pmm_reserve_range(heap_start, pages * PMM_FRAME_SIZE);
+    kmalloc_init(heap_start, pages * PMM_FRAME_SIZE);
+  }
+
   timer_init(TIMER_FREQ);
 
   asm volatile("sti"); // enable interrupts
