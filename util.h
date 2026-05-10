aefb143035db289f47df691b2670e68f805835fd
grae ceney <gceney7@gmail.com>
2026-05-10 18:18:22 +0100

    added some things that are almost allways used to utils to save includes

--- util.h
+++ util.h
@@ -7,4 +7,8 @@ size_t strlen(const char *str);
 void *memcpy(void *dest, const void *src, size_t count);
 void *memset(void *dest, int val, size_t count);
 
+#include "drivers/terminal.h"
+#include "mem/kmalloc.h"
+#include "mem/pmm.h"
+
 #endif
